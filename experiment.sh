caf7f233da5adddb8393e60211363258fbdbdd32
gbecan <guillaume.becan@irisa.fr>
2014-08-22 17:00:01 +0200

    add parameters in script + save intermediate files in output dir

--- experiment.sh
+++ experiment.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Init environment
-export PATH=$PATH:/usr/local/sicstus4.3.0/bin
+export PATH=$PATH:/usr/local/sicstus4.3.0/bin/
 export PATH=$PATH:/usr/local/scala/bin
 export http_proxy=http://proxy:3128/ && export https_proxy=https://proxy:3128/
 #cp /home/gbecan/afm-synthesis/
 
 # Perform experiment
 scala -classpath "jars/lib-afmsynthesis/*" jars/afmsynthesis_2.10-0.1.jar \
-results 2 3 4 true
+results $1 $2 $3 $4
 
 # Save results
 
