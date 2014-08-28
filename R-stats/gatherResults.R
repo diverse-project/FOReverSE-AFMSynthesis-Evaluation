5aff504bd726c6286e780936068f437212eb13be
gbecan <guillaume.becan@irisa.fr>
2014-08-28 17:51:23 +0200

    plots

--- R-stats/gatherResults.R
+++ R-stats/gatherResults.R
@@ -1,3 +1,5 @@
+require(ggplot2)
+require(reshape)
 
 readCSV <- function(dir,path) {
   fullPath = paste(dir,path,sep="")
@@ -11,5 +13,30 @@ readCSV <- function(dir,path) {
 dirs <- list.dirs("../results")[-1]
 files <- lapply(dirs, function(dir) readCSV(dir, "/metrics.csv"))
 results <- Reduce(function(...) merge(..., all=T), files)
-scalF <- results[,c("#features", "Synthesis")]
-plot(scalF)
+
+# Scalability over features
+scalF <- results[,c("#variables", "Synthesis", "Sicstus")] 
+sqrtScalF <- data.frame(scalF[1], lapply(scalF[2], sqrt), lapply(scalF[3], sqrt))
+
+#par(pch=18)
+plot(sqrtScalF$"X.variables", sqrtScalF$"Synthesis", xlab="Number of variables", ylab="Time", cex=0.8, pch=18)
+#points(sqrtScalF$"X.variables", sqrtScalF$"Sicstus")
+
+#p <- ggplot(sqrtScalF, aes(`#variables`, Synthesis))
+#p <- p + geom_point()
+#print(p)
+
+# Scalability over configurations
+scalC <- results[results$"#variables" == 10 & results$"max domain size" == 10 & results$"enable or groups" == 'false',]
+scalC <- scalC[,c("#configurations", "Synthesis")]
+
+plot(scalC$"#configurations", scalC$"Synthesis", xlab="Number of configurations", ylab="Time", cex=0.8, pch=18)
+
+# Scalability over max number of domain values
+scalD <- results[,c("max domain size", "Synthesis")]
+plot(scalD$"max domain size", scalD$"Synthesis", xlab="Maximum domain size", ylab="Time", cex=0.8, pch=18)
+
+
+
+
+
