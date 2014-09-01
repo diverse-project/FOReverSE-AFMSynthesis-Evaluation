d45406afd8931e35f8d41c5bfe87a1785973674c
Guillaume Becan <gbecan@frennes>
2014-09-01 18:34:59 +0200

    list products for a set of directoroes

--- list_products.sh
+++ list_products.sh
@@ -1,17 +1,34 @@
 #!/bin/sh
 
 # PARAMS : 
-# - directory
 # - timeout
+# - directories
 
-for dir in $1/*/
+# Init environment
+export PATH=$PATH:/usr/local/sicstus4.3.0/bin/
+export PATH=$PATH:/usr/local/scala/bin
+export http_proxy=http://proxy:3128/ && export https_proxy=https://proxy:3128/
+
+# Copy files for the experiment in /tmp/afm_experiment
+EXPERIMENT_DIR=/home/gbecan/afm-synthesis/FOReverSE-AFMSynthesis-Evaluation
+TMP_DIR=/tmp/afm_experiment
+#RESULTS_DIR=/data/gbecan_690668/afm-synthesis/results
+#LOG=/home/gbecan/afm-synthesis/results/log.txt
+
+mkdir $TMP_DIR
+cp -rf $EXPERIMENT_DIR/* $TMP_DIR
+cd $TMP_DIR
+
+# Perform experiment
+for dir in "${@:2}"
 do
     dir=${dir%*/}
-echo $dir
+	echo $dir	
 	# Generate products
-	java -Xmx16g -jar jars/MatrixGenerator.jar $dir $2
+	java -Xmx16g -jar jars/MatrixGenerator.jar $dir $1
 
 done
 
 
-
+# Clean /tmp
+rm -rf $TMP_DIR
