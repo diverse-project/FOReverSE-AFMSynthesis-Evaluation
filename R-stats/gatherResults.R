d08e2748d7ce08d2d4bcada1a6abd33b53bfa204
gbecan <guillaume.becan@irisa.fr>
2014-09-04 14:26:12 +0200

    backup of results + R scripts

--- R-stats/gatherResults.R
+++ R-stats/gatherResults.R
@@ -1,6 +1,7 @@
 source("utils.R")
 
-dirs <- list.dirs("/run/media/gbecan/SeagateExtHD/gbecan/afm-synthesis/results")[-1]
+dirs <- list.dirs("/run/media/gbecan/SeagateExtHD/gbecan/afm-synthesis/results_products")[-1]
+dirs <- c(dirs, list.dirs("/run/media/gbecan/SeagateExtHD/gbecan/afm-synthesis/results")[-1])
 files <- lapply(dirs, function(dir) readCSV(dir, "/metrics.csv"))
 results <- Reduce(function(...) merge(..., all=T), files)
 write.csv(results,"results.csv")
--- R-stats/plotResults.R
+++ R-stats/plotResults.R
@@ -4,17 +4,27 @@ results <- readCSV("", "results.csv")
 
 count <- split(results, list(results$"#variables", results$"#configurations", results$"max domain size", results$"enable or groups"), drop=TRUE)
 count <- lapply(count, function(x) nrow(x))
+print(count[count < 100])
+
+#### RQ1 ####
 
 # Scalability over features
+print("Scalability w.r.t features")
 scalF <- results[results$"#configurations" == 100 & results$"max domain size" == 10 & results$"enable or groups" == 'false',]
 scalF <- scalF[,c("#variables", "Synthesis", "Sicstus")] 
-scalF <- data.frame(scalF[1], lapply(scalF[2], function(x) sqrt(x/1000)), check.names = FALSE)
+scalF <- data.frame(scalF[1], lapply(scalF[2], function(x) (x/1000)**(1/2)), check.names = FALSE)
 plot(scalF$"#variables", scalF$"Synthesis", xlab="Number of variables", ylab="Square root of time", cex=0.8, pch=18, xaxt="n")
 axis(1, at = c(5, 50, 100, 200, 500, 1000, 2000), las=2)
 meanScalF <- aggregate(scalF$"Synthesis", by=list(scalF$"#variables"), FUN=mean)
 points(meanScalF$"Group.1", meanScalF$"x", pch=21, col="red", bg="red")
 
+scalF.lm <- lm(scalF$"Synthesis" ~ scalF$"#variables")
+scalF.r.squared <- summary(scalF.lm)$r.squared
+print(scalF.r.squared)
+abline(scalF.lm)
+
 # Scalability over configurations
+print("Scalability w.r.t configurations")
 scalC <- results[results$"#variables" == 50 & results$"max domain size" == 10 & results$"enable or groups" == 'false',]
 scalC <- scalC[,c("#distinct configurations", "Synthesis", "#configurations")]
 scalC <- data.frame(scalC[1], lapply(scalC[2], function(x) (x/1000)), scalC[3], check.names = FALSE)
@@ -23,7 +33,13 @@ axis(1, at = c(50, 200, 500, 1000, 2000, 5000, 10000), las=2)
 meanScalC <- aggregate(scalC$"Synthesis", by=list(scalC$"#configurations"), FUN=mean)
 points(meanScalC$"Group.1", meanScalC$"x", pch=21, col="red", bg="red")
 
+scalC.lm <- lm(scalC$"Synthesis" ~ scalC$"#distinct configurations")
+scalC.r.squared <- summary(scalC.lm)$r.squared
+print(scalC.r.squared)
+abline(scalC.lm)
+
 # Scalability over max number of domain values
+print("Scalability w.r.t max domain size")
 scalD <- results[results$"#variables" == 10 & results$"#configurations" == 10000 & results$"enable or groups" == 'false',]
 scalD <- scalD[,c("real max domain size", "Synthesis", "max domain size")]
 scalD <- data.frame(scalD[1], lapply(scalD[2], function(x) (x/1000)), scalD[3], check.names = FALSE)
@@ -31,6 +47,11 @@ plot(scalD$"real max domain size", scalD$"Synthesis", xlab="Maximum domain size"
 axis(1, at = c(5, 200, 500, 1000, 2000, 5000, 10000), las=2)
 meanScalD <- aggregate(data.frame(scalD$"Synthesis", scalD$"real max domain size"), by=list(scalD$"max domain size"), FUN=mean)
 points(meanScalD$"scalD..real.max.domain.size.", meanScalD$"scalD.Synthesis", pch=21, col="red", bg="red")
+
+scalD.lm <- lm(scalD$"Synthesis" ~ scalD$"real max domain size")
+scalD.r.squared <- summary(scalD.lm)$r.squared
+print(scalD.r.squared)
+abline(scalD.lm)
 
 # Scalability over features with OR groups
 scalFor <- results[results$"#configurations" == 1000 & results$"max domain size" == 10 & results$"enable or groups" == 'true',]
@@ -44,7 +65,6 @@ points(meanScalFor$"Group.1", meanScalFor$"x", pch=21, col="red", bg="red")
 
 
 # Bar plots
-#par(xpd=TRUE)
 filteredTimes <- results[results$"enable or groups" == 'false',]
 filteredTimes$"Feature and attribute extraction" <- filteredTimes$"Domain extraction" + filteredTimes$"Feature and attribute extraction"
 filteredTimes$"Other steps" <- 
@@ -84,6 +104,23 @@ barplot(times,
         #col=c("red","green"),
         legend = row.names(times), 
         horiz=TRUE, 
-        beside=FALSE
+        beside=FALSE,
+        xpd=TRUE
         )
 axis(1, at = seq(0, 1, 0.1), las=2)
+
+
+
+#### RQ2 ####
+overapprox <- na.omit(results)
+overapprox <- (overapprox$"#output configurations" - overapprox$"#distinct configurations") / overapprox$"#output configurations"
+#steps <- data.frame(c(0,seq(0,0.9,0.1)),c(0,seq(0.1,1,0.1)))
+#overapprox <- apply(steps, 1, function(d) (sum(overapprox > d[1] & overapprox <=d[2])) / NROW(overapprox))
+#barplot(overapprox, 
+        #horiz=TRUE, 
+        #beside=FALSE,
+        #names.arg=c("0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"),
+        #xaxt="n", 
+#        yaxt="n")
+#axis(2, at = seq(0,1,0.1), las=2)
+plot(ecdf(overapprox), xpd=FALSE, xlab="Overapproximation", ylab="Distribution", main="")
